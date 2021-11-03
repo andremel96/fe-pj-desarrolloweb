cbf788761580c333c00eba67701d930ac92726c2
Andrea Melgar <andimelgar96@gmail.com>
2021-11-02 23:13:35 -0600

    test2edit

--- public/api.js
+++ public/api.js
@@ -1,11 +1,11 @@
 const url = 'https://be-pj-desarrolloweb.herokuapp.com'
 let user_list = 'https://be-pj-desarrolloweb.herokuapp.com/users/'
 
-const generateRequest =(value, path, requestType) => {
-    return fethc (url + path),{ 
-        method: requestType, 
+const generateRequest = (value, path, requestType) => {
+    return fethc(url + path), {
+        method: requestType,
         body: JSON.stringify(fillobject(value)),
-        headers:{
+        headers: {
             'accept': 'application/json',
             'content-type': 'application/json'
         },
@@ -30,32 +30,30 @@ const generateRequest =(value, path, requestType) => {
 // }
 const cargarUsers = () => {
     fetch(
-        user_list 
+        user_list
     )
-      .then((res) => res.json())
-      .then((data) => {
-        main.innerHTML = "";
-        data.genres.forEach((item) => {
-        generateTable(item.id_UserName, item.iduserNumero, 
-            item.user_name, item.name, item.last_name);
+        .then((res) => res.json())
+        .then((data) => {
+            // main.innerHTML = "";
+            console.log(data);
+            const table = document.getElementById("UsersList");
+            table.innerHTML = "";
+            data.user.forEach((item) => {
+                generateTable(item, table);
+            });
         });
-      });
-  }
+}
 
-const generateTable = (data) => {
-    const table = document.getElementById("UsersList");
-    table.innerHTML = "";
-    data.replies[0].content.forEach(e => {
-        let row = table.insertRow();
-        let id_UserName = row.insertCell(0);
-        id_UserName.innerHTML = e.id_UserName;
-        let iduserNumero = row.insertCell(1);
-        iduserNumero.innerHTML = e.iduserNumero;
-        let user_name = row.insertCell(2);
-        user_name.innerHTML = e.user_name;
-        let name = row.insertCell(3);
-        name.innerHTML = e.name;
-        let last_name = row.insertCell(4);
-        last_name.innerHTML = e.last_name;
-    })
+const generateTable = (e, table) => {
+    let row = table.insertRow();
+    let id_UserName = row.insertCell(0);
+    id_UserName.innerHTML = e.id_UserName;
+    let iduserNumero = row.insertCell(1);
+    iduserNumero.innerHTML = e.iduserNumero;
+    let user_name = row.insertCell(2);
+    user_name.innerHTML = e.user_name;
+    let name = row.insertCell(3);
+    name.innerHTML = e.name;
+    let last_name = row.insertCell(4);
+    last_name.innerHTML = e.last_name;
 }
