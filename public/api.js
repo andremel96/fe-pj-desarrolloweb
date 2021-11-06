4a47168ac01ad90cbdd358679e1fd55580cf35fb
Andrea Melgar <andimelgar96@gmail.com>
2021-11-06 13:21:11 -0600

    modd

--- public/api.js
+++ public/api.js
@@ -1,6 +1,6 @@
 const url ='https://be-pj-desarrolloweb.herokuapp.com'; //'https://be-pj-desarrolloweb.herokuapp.com'
 let user_list = 'https://be-pj-desarrolloweb.herokuapp.com/users/'
-let curso_list = 'http://localhost:3001/curso/'
+let curso_list = 'https://be-pj-desarrolloweb.herokuapp.com/curso/'
 let carrera_list = 'https://be-pj-desarrolloweb.herokuapp.com/carrera/'
 let homework_list = 'https://be-pj-desarrolloweb.herokuapp.com/homeworks'
 
